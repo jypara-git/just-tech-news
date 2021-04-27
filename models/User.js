51fcf9ec99751bf41767c7affb6f53f521fe0ef5
jypara-git <zhyparaabdieva@gmail.com>
2021-04-27 15:24:13 -0700

    hashed passwords before create and update user

--- models/User.js
+++ models/User.js
@@ -46,7 +46,20 @@ User.init(
             }
         }
     },
+    // hooks are functions that are called  before or after calls in Sequilize
     {
+        hooks: {
+            // set up beforeCreate lifecycle "hook" functionality (10 is saltRound value)
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+            // set up beforeUpdate lifecycle "hook" functionality
+            async beforeUpdate(updatedUserData) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            }
+        },
         // TABLE CONFIGURATION OPTIONS GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration))
         // pass in our imported sequelize connection (the direct connection to our database) sequelize,
         sequelize,
