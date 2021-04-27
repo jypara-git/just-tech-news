1a035a2f343369fab54bd0b49a0340edfa09c29c
jypara-git <zhyparaabdieva@gmail.com>
2021-04-27 14:12:20 -0700

    installed bcrypt

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
 // create our User model (inherits all of the functionality the Model class has)
 class User extends Model {}
