19b5ca0dc72cabce50ce7a66a4dbf0e8f8b764df
jypara-git <zhyparaabdieva@gmail.com>
2021-04-28 16:47:21 -0700

    create the api routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,8 +3,10 @@ const { use } = require('./user-routes.js');
 
 const userRoutes = require('./user-routes.js');
 const postRoutes = require('./post-routes.js');
+const CommentRoutes = require('./comment-routes');
 
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
+router.use('/comments', CommentRoutes);
 
 module.exports = router;
